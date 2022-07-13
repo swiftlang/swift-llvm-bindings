3529caf274e3cd54649c2b5a00201c01ea9eafff
Egor Zhdan <e_zhdan@apple.com>
2022-07-13 17:55:19 +0100

    Add CMakeLists and a basic LLVM_Utils module

---
+++ Sources/LLVM_Utils/LLVM_Utils.swift
@@ -0,0 +1,23 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2022 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+@_exported import LLVM_Utils // Clang module
+
+extension String {
+  public func withStringRef<Result>(_ body: (llvm.StringRef) -> Result) -> Result {
+    let ref = llvm.StringRef(self)
+    let result = withExtendedLifetime(ref) {
+      body(ref)
+    }
+    return result
+  }
+}
